4267d5fd8796bb81efcdcc5a05467a2d74f98172
Arman229 <armanashraf015@gmail.com>
2024-04-12 18:35:26 +0500

    working on the authportal

--- AuthPortal/backend_authportal/backend_authportal/main.py
+++ AuthPortal/backend_authportal/backend_authportal/main.py
@@ -1,16 +1,49 @@
 from fastapi import FastAPI
 import uvicorn
+from backend_authportal import settings
+from sqlmodel import create_engine, Session
+from backend_authportal import model
+from contextlib import asynccontextmanager
 
 
+@asynccontextmanager
+async def beforeStarting(app: FastAPI):
+    print('before the table creation')
+    create_table()
+    yield
 
-app = FastAPI()
+
+app: FastAPI = FastAPI(lifespan=beforeStarting)
+
+
+def get_session():
+    with Session as session:
+        yield session
+
+
+connection_String: str = str(settings.DATA_BASE_URL).replace(
+    "postgresql", "postgresql + psycopg2")
+engine = create_engine(connection_String, connect_args={
+                       "sslmode": "require", }, pool_recycle=300)
+
+
+def create_table():
+    model.User.metadata.create_all(engine)
+
 
 @app.get('/')
 def home_city():
-    return {'message': 'Hello World'}
+    return {'message': 'Hello Wordld'}
+
+
+@app.get('/cities', response_model=dict[str, list[str]])
+def get_cities():
+    return {'Cities': ['lahore', 'sialkot', 'daska']}
+
 
 def main():
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)    
-    
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
 if __name__ == '__main__':
-    main()    
+    main()
---
+++ AuthPortal/backend_authportal/backend_authportal/model.py
@@ -0,0 +1,11 @@
+from sqlmodel import SQLModel,Field
+
+
+class User(SQLModel, table=True):
+  id: int = Field(default=None, primary_key=True)
+  username: str 
+  password: str 
+  email: str 
+  mobile_number: int
+ 
+  
