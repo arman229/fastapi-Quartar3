82784ce95cdb2511b8b1563516859b99c11a6483
Arman229 <armanashraf015@gmail.com>
2024-04-14 15:06:42 +0500

    query parmeters and string

--- fastapitiangolo/tutorialfastapi/tutorialfastapi/main.py
+++ fastapitiangolo/tutorialfastapi/tutorialfastapi/main.py
@@ -14,25 +14,49 @@ app: FastAPI = FastAPI(title='This is the tutorial of fastapi documentation')
 @app.get('/email')
 # A "decorator" takes the function below and does something with it.
 async def read_root(email: str) -> dict[str, str]:
-    
     return {"message": f"Hello World {email}"}
  
-
- 
+@app.get("/itemhidden/")
+async def read_items(
+    hidden_query: Annotated[str | None, Query(include_in_schema=False)] = None,
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    else:
+        return {"hidden_query": "Not found"}
 @app.get("/items/")
-# async def read_items(q: Annotated[str, Query(min_length=3)] =' ...'):
-    # Query parameter list / multiple values
-async def read_items(q: Annotated[Union[list[str], None], Query()] = None):
-    results:dict[str,list[dict[str,str]]|list[str]] = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+async def read_items(
+    q: Annotated[str | None, Query(title="Query string", min_length=3)] = None,
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
-    return results
+    return results 
+# @app.get("/items/")
+# # async def read_items(q: Annotated[str, Query(min_length=3)] =' ...'):
+#     # Query parameter list / multiple values
+# async def read_items(
+#     q: Annotated[
+#         str | None,
+#         Query(
+#             title="Query string",
+#             description="Query string for the items to search in the database that have a good match",
+#             min_length=3,
+#             alias='item-querys' ,
+#             deprecated=True,
+            
+#         ),
+#     ] = None,
+# ):
+#     results:dict[str,list[dict[str,str]]|list[str]] = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
 
  
 @app.get('/')
 # A "decorator" takes the function below and does something with it.
-async def read_root( ) -> dict[str, str]:
-    
+async def read_roots( ) -> dict[str, str]:
     return {"message": f"Hello World"}
 
 @app.get('/book_number/{book_id}')
@@ -50,17 +74,16 @@ class Language(str, Enum):
 @app.get('/language/{language}')
 async def read_language(language: Language) -> dict[str, str]:
     return {"language": language}
-
-
 # http://127.0.0.1:8000/files//home/johndoe/index.html
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
 
+
+
+
 def main():
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
-
 if __name__ == '__main__':
     main()
