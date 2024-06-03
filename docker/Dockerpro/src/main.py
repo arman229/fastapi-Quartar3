7332427bae1e64d066196ccde361d38b9964bb7f
Arman <armanashraf015@gmail.com>
2024-06-03 08:26:36 +0500

    I have successfully created a Poetry project, written a Dockerfile , built an image, and run it in a container

--- docker/Dockerpro/src/main.py
+++ docker/Dockerpro/src/main.py
@@ -6,7 +6,7 @@ def hello():
     return{ "message ":"hello World"}
 @app.get("/name")
 def name():
-   return{ "Name ":"My name is Arman ashraf "}    
+   return{ "Name ":"My name is Arman ashraf ansari "}    
 @app.get("/docker")
 def docker():
     return{ "Topic ":"Docker"}
